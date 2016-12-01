9d57c3898165603e121a91bef2aa70b956281cff
Adjamilton Junior <jr@ieee.org>
2016-12-01 13:04:12 -0300

    Update exemplo.

--- exemplo/js/main.js
+++ exemplo/js/main.js
@@ -1,4 +1,15 @@
-// Ler o JSON
+
+
 
 var value = document.querySelector('#search').value;
 console.log(value);
+
+ $.getJSON(‘select.json’, function(data) {
+        var output="<ul>";
+        for (var i in data.ResponseHeader) {
+            output+="<li>" + data.users[i].firstName + " " + data.users[i].lastName + "–" + data.users[i].joined.month+"</li>";
+        }
+
+        output+="</ul>";
+        document.getElementById("placeholder").innerHTML=output;
+  });
